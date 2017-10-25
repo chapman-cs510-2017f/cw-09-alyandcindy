f950ce690eb0179de14a0794a1ef12ef379a6ded
Cynthia Parks <cparks@chapman.edu>
2017-10-25 04:07:37 +0000

    fixed factorial.c

--- src/factorial.c
+++ src/factorial.c
@@ -5,6 +5,7 @@
  * their definitions appear. This prevents ordering issues during compilation.
  */
 #include "factorial.h"
+#include <stdio.h>
 
 /* Definition for "factorial"
  * Note: the type must match the type specified within the header file above
@@ -14,16 +15,23 @@ int factorial(int n) {
      * of each function. This includes all iteration dummy variables. */
     int fac = n;
     
-    while (n > 0) {
-        /* Note: The shorthand syntax
-         * var-- reads the contents of an integer variable, then decrements
-         * --var decrements the contents of an integer variable, then reads it
-         */
-        n--;
-        // a *= b is equivalent to a = a * b
-        fac *= n;
+    if (n == 0){
+        printf("0! = 1 \n");
+        return 1;
     }
-    
+    else{
+        while (n > 1){
+            /* Note: The shorthand syntax
+             * var-- reads the contents of an integer variable, then decrements
+             * --var decrements the contents of an integer variable, then reads it
+             */
+            n--;
+            printf("Fac is %d \n n is %d \n", fac, n);
+            // a *= b is equivalent to a = a * b
+            fac *= n;
+        printf("Fac is %d \n", fac);
+        }
+    }
     /* The type of the returned value must match the type indicated before the
      * function name above (here "int")
      */
